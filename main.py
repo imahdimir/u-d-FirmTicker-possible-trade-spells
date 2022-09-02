24233215754014a0f0686532fbe9f314287f44a9
imahdimir <imahdimir@gmail.com>
2022-09-02 14:45:52 +0430

    init

--- main.py
+++ main.py
@@ -4,78 +4,79 @@
 
 ##
 
-import sys
+from datetime import time
+from datetime import datetime as dt
 
-import pyspark.sql.functions as sfunc
+import pandas as pd
+from githubdata import GithubData
 from mirutil.df_utils import save_as_prq_wo_index as sprq
-from pyspark.sql import SparkSession
-from pyspark.sql import Window
-from pyspark.sql.functions import concat_ws
-
-from shared import _ret_df_of_every_second_in_day
-from shared import _ret_status_change_data
-from shared import _ret_tsetmc_id_data
-from shared import _ret_working_date_data
-from shared import c
 
 
-spark = SparkSession.builder.getOrCreate()
+class RepoAddresses :
+  tids = 'imahdimir/d-TSETMC_ID-2-Ticker'
+  wds = 'imahdimir/d-TSE-working-days'
+
+ra = RepoAddresses()
+
+class Constants :
+  mkt_start_time = time(9 , 00)
+  mkt_end_time = time(12 , 30)
+
+cte = Constants()
+
+class ColumnNames :
+  tid = 'TSETMC_ID'
+  jd = 'JDate'
+  d = 'Date'
+  st = 'StartTime'
+  et = 'EndTime'
+  dur = 'Duration'
+  sdt = 'StartDateTime'
+  edt = 'EndDateTime'
+  sjdt = 'StartJDateTime'
+  ejdt = 'EndJDateTime'
+
+c = ColumnNames()
 
 def main() :
 
   pass
 
   ##
-  dst = _ret_status_change_data()
-  dtw = _ret_working_date_data()
-  did = _ret_tsetmc_id_data()
+  rp_tid = GithubData(ra.tids)
+  df_tid = rp_tid.read_data()
   ##
-  dti = _ret_df_of_every_second_in_day()
+  df_tid.reset_index(inplace = True)
   ##
-  sdt = spark.createDataFrame(dti)
-  sdw = spark.createDataFrame(dtw)
+  df_tid = df_tid[[c.tid]]
   ##
-  for tid in did[c.id] :
-    print(tid)
 
-    did1 = did[did[c.id].eq(tid)]
+  rp_wds = GithubData(ra.wds)
+  df_wds = rp_wds.read_data()
+  ##
+  df_wds.reset_index(inplace = True)
+  ##
+  df_wds = df_wds[[c.jd , c.d]]
+  ##
+  for cn , val in zip([c.st , c.et] , [cte.mkt_start_time , cte.mkt_end_time]) :
+    df_wds[cn] = val
+  ##
+  _adc = {
+      c.sjdt : lambda x : str(x[c.jd]) + ' ' + str(x[c.st]) ,
+      c.ejdt : lambda x : str(x[c.jd]) + ' ' + str(x[c.et]) ,
+      c.sdt  : lambda x : str(x[c.d]) + ' ' + str(x[c.st]) ,
+      c.edt  : lambda x : str(x[c.d]) + ' ' + str(x[c.et]) ,
+      }
 
-    sdi = spark.createDataFrame(did1)
+  for ky , val in _adc.items() :
+    df_wds[ky] = df_wds.apply(val , axis = 1)
 
-    sd = sdi.crossJoin(sdw)
-    sd = sd.crossJoin(sdt)
+  ##
+  df = pd.merge(df_tid , df_wds , how = 'cross')
 
-    newcol = concat_ws(' ' , sd.Date , sd.Time).alias(c.dt)
-    sd = sd.select(c.id , newcol , c.ismktopen)
+  ##
+  sprq(df , 'temp.prq')
 
-    sds = spark.createDataFrame(dst)
-
-    sd = sd.join(sds , [c.id , c.dt] , how = 'outer')
-
-    sd = sd.sort([c.id , c.dt])
-
-    window = Window.partitionBy(c.id).orderBy(c.dt).rowsBetween(-sys.maxsize ,
-                                                                0)
-    filled_column = sfunc.last(sd[c.trdble] , ignorenulls = True).over(window)
-    sdf_filled = sd.withColumn('filled' , filled_column)
-
-    sd = sdf_filled
-
-    msk = sd['filled'] == True
-    msk &= sd[c.ismktopen] == True
-    sd = sd.filter(msk)
-
-    sd = sd.select(c.id , c.dt)
-
-    sd = sd.withColumn(c.d , sfunc.substring(c.dt , 1 , 10))
-
-    sd = sd.groupBy([c.id , c.d]).count()
-
-    sdv = sd.toPandas()
-
-    sprq(sdv , f'{tid}.prq')
-
-    break
 
   ##
 
@@ -111,14 +112,5 @@ if __name__ == '__main__' :
   print('done')
 
 ##
-import dulwich
-
-
-##
-url = 'https://github.com/imahdimir/d-clean-d-firm-status-change'
-dulwich.porcelain.clone(url)
-
-##
-
 
 ##
